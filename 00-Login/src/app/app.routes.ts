358e9b0a394f6e800bce81fd23a0cf87004a2b84
Ryan Chenkie <ryanchenkie@gmail.com>
2017-05-16 15:21:54 -0400

    stage embedded login

--- 00-Login/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { CallbackComponent } from './callback/callback.component';
-
-export const ROUTES: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'callback', component: CallbackComponent },
-  { path: '**', redirectTo: '' }
-];
