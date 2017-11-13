66c1e8fe7bc60d6031761f3172d170f80458f0b5
Luís Rudge <luis@luisrudge.net>
2017-11-13 14:41:06 -0200

    remove extra redirect

--- 02-Custom-Login-Form/src/app/auth/auth.service.ts
+++ 02-Custom-Login-Form/src/app/auth/auth.service.ts
@@ -58,7 +58,6 @@ export class AuthService {
     this.auth0.parseHash((err, authResult) => {
       if (authResult && authResult.accessToken && authResult.idToken) {
         this.setSession(authResult);
-        this.router.navigate(['/home']);
       } else if (err) {
         this.router.navigate(['/home']);
         console.log(err);
