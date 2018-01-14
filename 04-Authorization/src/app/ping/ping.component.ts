7b3df2dfd1f7e5241e19bebd4da34174ee1d9d9c
Siva Kandaraj <siva.kandaraj@gmail.com>
2018-01-14 22:08:03 +1100

    Added auth service to ping component
    - Without AuthService, pingcomponent.html will throw error with *auth* not found

--- 04-Authorization/src/app/ping/ping.component.ts
+++ 04-Authorization/src/app/ping/ping.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthHttp } from 'angular2-jwt';
 import { Http } from '@angular/http';
+import { AuthService } from './../auth/auth.service';
 import 'rxjs/add/operator/map';
 
 @Component({
@@ -13,7 +14,7 @@ export class PingComponent implements OnInit {
   API_URL = 'http://localhost:3001/api';
   message: string;
 
-  constructor(public http: Http, public authHttp: AuthHttp) {}
+  constructor(public http: Http, public authHttp: AuthHttp, public auth: AuthService) {}
 
   ngOnInit() {
   }
