4ccf85f0f1437b3af6160bb3d153f46fdacc8e64
Steve Hobbs <elkdanger@gmail.com>
2019-04-28 21:48:38 +0100

    Added server.js

---
+++ 03-Calling-an-API/server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const jwt = require('express-jwt');
+const jwksRsa = require('jwks-rsa');
+const { join } = require('path');
+const authConfig = require('./auth_config.json');
+
+const app = express();
+
+if (!authConfig.domain || !authConfig.audience) {
+  throw 'Please make sure that auth_config.json is in place and populated';
+}
+
+app.use(morgan('dev'));
+app.use(helmet());
+
+const checkJwt = jwt({
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://${authConfig.domain}/.well-known/jwks.json`
+  }),
+
+  audience: authConfig.audience,
+  issuer: `https://${authConfig.domain}/`,
+  algorithm: ['RS256']
+});
+
+app.get('/api/external', checkJwt, (req, res) => {
+  res.send({
+    msg: 'Your access token was successfully validated!'
+  });
+});
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(join(__dirname, 'dist')));
+
+  app.use((_, res) => {
+    res.sendFile(join(__dirname, 'dist', 'index.html'));
+  });
+}
+
+module.exports = app;
