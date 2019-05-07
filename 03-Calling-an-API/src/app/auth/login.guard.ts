06b888eb2ed014c31cab7a1e7a64ec1dfb412f80
Steve Hobbs <elkdanger@gmail.com>
2019-05-07 15:25:39 +0100

    Installed bootstrap theme

--- 03-Calling-an-API/src/app/auth/login.guard.ts
+++ 03-Calling-an-API/src/app/auth/login.guard.ts
@@ -30,7 +30,7 @@ export class LoginGuard implements CanActivate {
         }
 
         client.loginWithRedirect({
-          redirect_uri: `${window.location.origin}/callback`
+          redirect_uri: `${window.location.origin}/callback`,
           appState: { target: next.url[0].path }
         });
 
