2a0fb40694780542ba9c6e0e49524566551206e9
Kim Maida <kim.maida@auth0.com>
2019-08-23 09:45:51 -0400

    Remove unsubscribe call for subscriptions to observables that complete

--- 01-Login/src/app/auth/auth.service.ts
+++ 01-Login/src/app/auth/auth.service.ts
@@ -63,12 +63,10 @@ export class AuthService {
         return of(loggedIn);
       })
     );
-    const checkAuthSub = checkAuth$.subscribe((response: { [key: string]: any } | boolean) => {
+    checkAuth$.subscribe((response: { [key: string]: any } | boolean) => {
       // If authenticated, response will be user object
       // If not authenticated, response will be 'false'
       this.loggedIn = !!response;
-      // Clean up subscription
-      checkAuthSub.unsubscribe();
     });
   }
 
@@ -105,11 +103,9 @@ export class AuthService {
     );
     // Subscribe to authentication completion observable
     // Response will be an array of user and login status
-    const authCompleteSub = authComplete$.subscribe(([user, loggedIn]) => {
+    authComplete$.subscribe(([user, loggedIn]) => {
       // Redirect to target route after callback processing
       this.router.navigate([targetRoute]);
-      // Clean up subscription
-      authCompleteSub.unsubscribe();
     });
   }
 
--- 02-Calling-an-API/src/app/pages/external-api/external-api.component.ts
+++ 02-Calling-an-API/src/app/pages/external-api/external-api.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ApiService } from 'src/app/api.service';
-import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-external-api',
   templateUrl: './external-api.component.html',
   styleUrls: ['./external-api.component.css']
 })
-export class ExternalApiComponent implements OnInit, OnDestroy {
+export class ExternalApiComponent implements OnInit {
   responseJson: string;
-  pingSub: Subscription;
 
   constructor(private api: ApiService) { }
 
@@ -17,15 +15,9 @@ export class ExternalApiComponent implements OnInit, OnDestroy {
   }
 
   pingApi() {
-    this.pingSub = this.api.ping$().subscribe(
+    this.api.ping$().subscribe(
       res => this.responseJson = JSON.stringify(res, null, 2).trim()
     );
-  }
-
-  ngOnDestroy() {
-    if (this.pingSub) {
-      this.pingSub.unsubscribe();
-    }
   }
 
 }
