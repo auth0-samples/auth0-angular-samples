8b7791d91dffa8f53b473e446e3ba2daa5329bb3
Steve Hobbs <steve.hobbs.mail@gmail.com>
2020-08-13 11:49:40 +0100

    Fix docker commands in script files

--- Sample-01/exec.ps1
+++ Sample-01/exec.ps1
@@ -1,3 +1,2 @@
-#!/usr/bin/env bash
-docker build -t auth0-angular-02-api .
-docker run --init -p 3000:3000 -it auth0-angular-02-api
+docker build -t auth0-angular-sample .
+docker run --init -p 4200:4200 -it auth0-angular-sample
--- Sample-01/exec.sh
+++ Sample-01/exec.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-docker build -t auth0-angular-02-login .
-docker run --init -p 3000:3000 -it auth0-angular-02-login
+docker build -t auth0-angular-sample .
+docker run --init -p 4200:4200 -it auth0-angular-sample
