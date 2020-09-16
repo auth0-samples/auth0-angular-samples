f7b59b88955eae63399bcec49f5b9c0e1c3eaea4
Steve Hobbs <steve.hobbs.mail@gmail.com>
2020-09-16 11:45:51 +0100

    Use inject instead of get

--- Sample-01/src/app/api.service.spec.ts
+++ Sample-01/src/app/api.service.spec.ts
@@ -6,7 +6,7 @@ describe('ApiService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
-    const service: ApiService = TestBed.get(ApiService);
+    const service: ApiService = TestBed.inject(ApiService);
     expect(service).toBeTruthy();
   });
 });
