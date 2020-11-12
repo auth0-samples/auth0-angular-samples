0df9d45cab979c205cb1e8cf37dd5f8f36feff45
frederikprijck <frederik.prijck@auth0.com>
2020-11-12 08:28:38 +0100

    Update Sample application to use Angular 11

--- Sample-01/src/app/pages/external-api/external-api.component.spec.ts
+++ Sample-01/src/app/pages/external-api/external-api.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { ExternalApiComponent } from './external-api.component';
