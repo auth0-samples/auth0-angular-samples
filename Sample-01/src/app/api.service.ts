9a42290165a41f7594ad0d7309ab4c2c533c993b
Frederik Prijck <frederik.prijck@auth0.com>
2022-09-21 15:01:21 +0200

    Update Sample to Angular 14 (#296)

--- Sample-01/src/app/api.service.ts
+++ Sample-01/src/app/api.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import * as config from '../../auth_config.json';
+import config from '../../auth_config.json';
 
 @Injectable({
   providedIn: 'root',
