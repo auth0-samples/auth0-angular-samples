b9055e2852467d36e08876068a27be736fa10ef7
frederikprijck <frederik.prijck@auth0.com>
2023-03-13 09:48:19 +0100

    Move to standalone folder

--- Standalone/src/app/app.component.ts
+++ Standalone/src/app/app.component.ts
@@ -2,20 +2,28 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { filter, mergeMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   standalone: true,
-  imports: [
-    RouterOutlet,
-    NavBarComponent,
-    FooterComponent
-  ]
+  imports: [RouterOutlet, NavBarComponent, FooterComponent],
 })
 export class AppComponent {
   title = 'Auth0 Angular SDK Sample';
 
-  constructor() {}
+  constructor(private readonly auth: AuthService) {}
+
+  ngOnInit(): void {
+    this.auth.error$
+      .pipe(
+        filter(
+          (e) => e instanceof GenericError && e.error === 'login_required'
+        ),
+        mergeMap(() => this.auth.loginWithRedirect())
+      )
+      .subscribe();
+  }
 }
