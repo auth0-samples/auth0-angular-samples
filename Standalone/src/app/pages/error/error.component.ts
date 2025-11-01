0724222346259611cfa521e747e1cc4160e3e120
Nagesh Andani <nagesh.andani@gmail.com>
2025-11-01 14:30:30 +0530

    chore: refactor *ngIf to use @if syntax

--- Standalone/src/app/pages/error/error.component.ts
+++ Standalone/src/app/pages/error/error.component.ts
@@ -1,25 +1,30 @@
-import { AsyncPipe, NgIf } from '@angular/common';
+import { AsyncPipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '@auth0/auth0-angular';
 import { Observable, timer } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 
+interface Auth0Error extends Error {
+  error_description?: string;
+  error?: string;
+  error_uri?: string;
+}
+
 @Component({
   selector: 'app-error',
   templateUrl: './error.component.html',
   standalone: true,
   imports: [
-    AsyncPipe,
-    NgIf
+    AsyncPipe
   ]
 })
 export class ErrorComponent implements OnInit {
 
-  public error$: Observable<Error>;
+  public error$: Observable<Auth0Error>;
 
   constructor(private auth: AuthService, private router: Router) {
-    this.error$ = this.auth.error$;
+    this.error$ = this.auth.error$ as Observable<Auth0Error>;
   }
 
   ngOnInit() {
