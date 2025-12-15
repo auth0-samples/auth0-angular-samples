08519d8659d34789a9d2fe0f2753c208027b8d23
Nicolas Garcia <nicolas.garcia@okta.com>
2025-12-15 12:00:59 -0300

    working app with config from public/config.js

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,10 +2,18 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideAuth0 } from '@auth0/auth0-angular';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    provideAuth0({
+      domain: window.AUTH0_DOMAIN,
+      clientId: window.AUTH0_CLIENT_ID,
+      authorizationParams: {
+        redirect_uri: window.location.origin,
+      },
+    }),
+  ],
 };
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,16 +1,34 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+/* highlight-start import-auth0 */
+import { AuthService } from '@auth0/auth0-angular';
+import { CommonModule } from '@angular/common';
+/* highlight-end import-auth0 */
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
+  // highlight-start auth0-usage
   template: `
     <main>
-      hello world!
+      @if (auth.isLoading$ | async) { Loading... } @else if (auth.isAuthenticated$ | async) { @if
+      (auth.user$ | async; as user) {
+      <p>Logged in as {{ user.email }}</p>
+      <h1>User Profile</h1>
+      <pre>{{ user | json }}</pre>
+      <button (click)="auth.logout()">Logout</button>
+      } } @else {
+      <button (click)="auth.loginWithRedirect({ authorizationParams: { screen_hint: 'signup' } })">
+        Sign Up
+      </button>
+      <button (click)="auth.loginWithRedirect()">Log In</button>
+      }
       <router-outlet />
     </main>
   `,
+  // highlight-end auth0-usage
 })
 export class App {
-  protected readonly title = signal('auth0-angular');
+  protected auth = inject(AuthService);
 }
